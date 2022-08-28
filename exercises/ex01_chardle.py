aeb61b0c7d8a956da62480f03abffbcf4ee7e6bc
Taylor Born <tnborn@unc.edu>
2022-08-28 10:48:39 -0400

    +wordle

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,32 @@
+"""EX01 - Chardle - Wordle"""
+__author__ = "730563181"
+
+
+original_word: str = input("Enter a 5-character word: ")
+if len(original_word) != 5:
+    exit("Error: Word must contain 5 characters")
+character_one: str = input("Enter a single character: ")
+if len(character_one) != 1:
+    exit("Error: Character must be a single character.")
+print("Searching for " + character_one + " in " + original_word)
+if character_one == original_word[0]:
+    print(character_one + " found at index 0")
+if character_one == original_word[1]:
+    print(character_one + " found at index 1")
+if character_one == original_word[2]:
+    print(character_one + " found at index 2")
+if character_one == original_word[3]:
+    print(character_one + " found at index 3")
+if character_one == original_word[4]:
+    print(character_one + " found at index 4")
+
+counter = 0
+for c in original_word:
+    if c == character_one:
+         counter += 1
+if counter >= 2:
+    print(counter, " instances of " + character_one + " found in " + original_word)
+if counter == 1:
+    print(counter, " instance of " + character_one + " found in " + original_word)
+if counter == 0: 
+    print("No instances of " + character_one + " found in " + original_word)
